61c6a683388d7699ef5364506efa16205aad567c
Tsarkov Ilya <itsarkov.kavolearn@yandex.ru>
2022-03-26 23:52:12 +0300

    add django template

--- kavolearn/articles/urls.py
+++ kavolearn/articles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('articles/base_syntax', views.show_article_base_syntax, name='show_article_base_syntax'),
     path('articles/oop', views.show_article_oop, name='show_article_oop'),
+    path('articles/django', views.show_article_django, name='show_article_django'),
 ]
--- kavolearn/articles/views.py
+++ kavolearn/articles/views.py
@@ -9,3 +9,7 @@ def show_article_base_syntax(request):
 
 def show_article_oop(request):
     return render(request, 'articles/show_article_oop.html')
+
+
+def show_article_django(request):
+    return render(request, 'articles/show_article_django.html')
